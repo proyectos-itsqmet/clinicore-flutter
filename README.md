4d3e68a0e03af81648008a9a71ea1459e454ab61
Gerardo Araque <gerard.araq@gmail.com>
2026-08-20 21:12:35 -0500

    Feat: CliniCore

---
+++ README.md
@@ -0,0 +1,3 @@
+# clinicore_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/clinicore_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clinicore_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
